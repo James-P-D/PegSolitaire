f77c9a872be4af3c2a3eff6dd82ca121c90b616b
James Dorrington <jdorrington@hotmail.com>
2020-06-27 17:59:14 +0100

    Renaming constants

--- src/solitaire.ml
+++ src/solitaire.ml
@@ -1,36 +1,35 @@
 exception SolitaireException of string;;
     
-let __VOID__ = 0;;
-let _EMPTY__ = 1;;
-let _MARBLE_ = 2;;
+let void = 0;;
+let empty = 1;;
+let marble = 2;;
 
 let _HEIGHT = 7;;
 let _WIDTH = 7;;
 
-let start_board = [[__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
-                   [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
-                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
-                   [_MARBLE_;_MARBLE_;_MARBLE_;_EMPTY__;_MARBLE_;_MARBLE_;_MARBLE_];
-                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
-                   [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
-                   [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__]];;
+let start_board = [[void;   void;   marble; marble; marble; void;   void];
+                   [void;   void;   marble; marble; marble; void;   void];
+                   [marble; marble; marble; marble; marble; marble; marble];
+                   [marble; marble; marble; empty;  marble; marble; marble];
+                   [marble; marble; marble; marble; marble; marble; marble];
+                   [void;   void;   marble; marble; marble; void;   void];
+                   [void;   void;   marble; marble; marble; void;   void]];;
 
 (*
 let _HEIGHT = 5;;
 let _WIDTH = 5;;
 
-let start_board = [[__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__];
-                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
-                   [_MARBLE_;_MARBLE_;_EMPTY__;_MARBLE_;_MARBLE_];
-                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
-                   [__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__]];;
+let start_board = [[void;   marble; marble; marble; void];
+                   [marble; marble; marble; marble; marble];
+                   [marble; marble; empty;  marble; marble];
+                   [marble; marble; marble; marble; marble];
+                   [void;   marble; marble; marble; void]];;
 *)
 
 let int_to_string i =
-  if (i == _MARBLE_) then  " * " else (
-    if (i == _EMPTY__) then " - " else "   ";
+  if (i == marble) then  " O " else (
+    if (i == empty) then " - " else "   ";
   );;
-
   
 let get_row b y = List.nth b y;;  
 let get_item b x y = List.nth (get_row b y) x;;
@@ -49,18 +48,19 @@ let draw_board board =
     done;
     Printf.printf "\n";
   done;
+  Printf.printf "\n";
   flush stdout;;  
 
 let is_complete board = 
   let total_marbles = ref 0 in
   for y = 0 to (_HEIGHT - 1) do 
     for x = 0 to (_WIDTH - 1)  do
-      if ((get_item board x y) == _MARBLE_) then (
+      if ((get_item board x y) == marble) then (
         total_marbles := (!total_marbles) + 1;
       )
     done;
   done;
-  (!total_marbles) < 4;;
+  (!total_marbles) == 1;;
 
 let apply_move board x1 y1 x2 y2 =
   (* Printf.printf "(%d, %d) -> (%d, %d)\n" x1 y1 x2 y2; *)
@@ -71,14 +71,14 @@ let apply_move board x1 y1 x2 y2 =
   for y = 0 to (_HEIGHT - 1) do 
     for x = 0 to (_WIDTH - 1) do
       if ((x == x1) && (y == y1)) then 
-        new_row := _EMPTY__ :: (!new_row)
+        new_row := empty :: (!new_row)
       else (
         if ((x == middle_x) && (y == middle_y)) then
           (
-            new_row := _EMPTY__ :: (!new_row)
+            new_row := empty :: (!new_row)
           ) else (
           if ((x == x2) && (y == y2)) then (
-            new_row := _MARBLE_ :: (!new_row)
+            new_row := marble :: (!new_row)
           ) else (
             new_row := (get_item board x y) :: (!new_row);
           )
@@ -95,8 +95,8 @@ let apply_move board x1 y1 x2 y2 =
 let make_move board x1 y1 x2 y2 = 
   let move_piece = get_item board x1 y1 in
   let target_pos = get_item board x2 y2 in
-  if (move_piece != _MARBLE_) then (raise (SolitaireException ("(" ^ string_of_int(x1) ^ ", " ^ string_of_int(y1) ^ ") is not a marble" ))) else (
-    if (target_pos != _EMPTY__) then (raise (SolitaireException ("(" ^ string_of_int(x2) ^ ", " ^ string_of_int(y2) ^ ") is not empty" ))) else (
+  if (move_piece != marble) then (raise (SolitaireException ("(" ^ string_of_int(x1) ^ ", " ^ string_of_int(y1) ^ ") is not a marble" ))) else (
+    if (target_pos != empty) then (raise (SolitaireException ("(" ^ string_of_int(x2) ^ ", " ^ string_of_int(y2) ^ ") is not empty" ))) else (
       apply_move board x1 y1 x2 y2;
     );
   );;
@@ -105,39 +105,39 @@ let make_move board x1 y1 x2 y2 =
 let get_moves board x y =
   let moves = ref [] in
   let move_piece = get_item board x y in
-  if (move_piece == _MARBLE_) then (
+  if (move_piece == marble) then (
     if ((x - 2) >= 0) then (
       let jump_over = get_item board (x - 1) y in
       let target = get_item board (x - 2) y in
-      if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
+      if ((jump_over == marble) && (target == empty)) then (
         moves := (x - 2, y) :: (!moves);
       )
     );
     if ((x + 2) < _WIDTH) then (
       let jump_over = get_item board (x + 1) y in
       let target = get_item board (x + 2) y in
-      if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
+      if ((jump_over == marble) && (target == empty)) then (
         moves := (x + 2, y) :: (!moves);
       )
     );
     if ((y - 2) >= 0) then (
       let jump_over = get_item board x (y - 1) in
       let target = get_item board x (y - 2) in
-      if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
+      if ((jump_over == marble) && (target == empty)) then (
         moves := (x, y - 2) :: (!moves);
       )
     );
     if ((y + 2) < _HEIGHT) then (
       let jump_over = get_item board x (y + 1) in
       let target = get_item board x (y + 2) in
-      if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
+      if ((jump_over == marble) && (target == empty)) then (
         moves := (x, y + 2) :: (!moves);
       )
     );
   );
   moves;;
   
-let rec solve board n =
+let rec solve board =
   (*let str = input_line stdin in*)
   if (is_complete board) then (
     draw_board board;
@@ -145,16 +145,11 @@ let rec solve board n =
   ) else (
     for y = 0 to (_HEIGHT - 1) do 
       for x = 0 to (_WIDTH - 1) do
-        if (n < 3) then (
-          Printf.printf "%d - %d,%d\n" n x y;
-          flush stdout;
-        );
-
         let possible_moves = get_moves board x y in
         for i = 0 to ((List.length (!possible_moves)) - 1) do
           let (x2, y2) = List.nth (!possible_moves) i in
           let new_board = apply_move board x y x2 y2 in
-          solve (!new_board) (n + 1)
+          solve (!new_board)
         done;
       done;
     done;
@@ -163,4 +158,4 @@ let rec solve board n =
   
 let do_stuff =
   draw_board start_board;
-  solve start_board 0;;
+  solve start_board;;
