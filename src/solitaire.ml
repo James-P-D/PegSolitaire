5fc5eec357e9bcf4e039e7952f322bc6f4cf41f7
James Dorrington <jdorrington@hotmail.com>
2020-06-27 17:28:59 +0100

    Fixing printing of board, actually using HEIGHT and WIDTH, adding new, smaller board.

--- src/solitaire.ml
+++ src/solitaire.ml
@@ -4,11 +4,8 @@ let __VOID__ = 0;;
 let _EMPTY__ = 1;;
 let _MARBLE_ = 2;;
 
-let _HEIGHT = 6;;
-let _WIDTH = 6;;
-
-let int_to_string i =
-  if (i = _MARBLE_) then  " O " else "   ";;
+let _HEIGHT = 7;;
+let _WIDTH = 7;;
 
 let start_board = [[__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
                    [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
@@ -17,42 +14,62 @@ let start_board = [[__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID
                    [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
                    [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__];
                    [__VOID__;__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__;__VOID__]];;
+
+(*
+let _HEIGHT = 5;;
+let _WIDTH = 5;;
+
+let start_board = [[__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__];
+                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
+                   [_MARBLE_;_MARBLE_;_EMPTY__;_MARBLE_;_MARBLE_];
+                   [_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_;_MARBLE_];
+                   [__VOID__;_MARBLE_;_MARBLE_;_MARBLE_;__VOID__]];;
+*)
+
+let int_to_string i =
+  if (i == _MARBLE_) then  " * " else (
+    if (i == _EMPTY__) then " - " else "   ";
+  );;
+
   
 let get_row b y = List.nth b y;;  
 let get_item b x y = List.nth (get_row b y) x;;
 
 let draw_board board = 
-  Printf.printf "    0  1  2  3  4  5  6\n";
-  Printf.printf "        +---------+\n";
-  Printf.printf "0       |%s%s%s|\n"       (int_to_string(get_item board 2 0)) (int_to_string(get_item board 3 0)) (int_to_string(get_item board 4 0));
-  Printf.printf "1 +-----+%s%s%s+-----+\n" (int_to_string(get_item board 2 1)) (int_to_string(get_item board 3 1)) (int_to_string(get_item board 4 1));
-  Printf.printf "2 |%s%s%s%s%s%s%s|\n"     (int_to_string(get_item board 0 2)) (int_to_string(get_item board 1 2)) (int_to_string(get_item board 2 2)) (int_to_string(get_item board 3 2)) (int_to_string(get_item board 4 2)) (int_to_string(get_item board 5 2)) (int_to_string(get_item board 6 2));
-  Printf.printf "3 |%s%s%s%s%s%s%s|\n"     (int_to_string(get_item board 0 3)) (int_to_string(get_item board 1 3)) (int_to_string(get_item board 2 3)) (int_to_string(get_item board 3 3)) (int_to_string(get_item board 4 3)) (int_to_string(get_item board 5 3)) (int_to_string(get_item board 6 3));
-  Printf.printf "4 |%s%s%s%s%s%s%s|\n"     (int_to_string(get_item board 0 4)) (int_to_string(get_item board 1 4)) (int_to_string(get_item board 2 4)) (int_to_string(get_item board 3 4)) (int_to_string(get_item board 4 4)) (int_to_string(get_item board 5 4)) (int_to_string(get_item board 6 4));
-  Printf.printf "5 +-----+%s%s%s+-----+\n" (int_to_string(get_item board 2 5)) (int_to_string(get_item board 3 5)) (int_to_string(get_item board 4 5));
-  Printf.printf "6       |%s%s%s|\n"       (int_to_string(get_item board 2 6)) (int_to_string(get_item board 3 6)) (int_to_string(get_item board 4 6));
-  Printf.printf "        +---------+\n";    
-  flush stdout;;
+  Printf.printf "  ";
+  for x = 0 to (_WIDTH - 1) do
+    Printf.printf " %d " x;
+  done;
+  Printf.printf "\n";
+    
+  for y = 0 to (_HEIGHT - 1) do 
+    Printf.printf "%d " y;
+    for x = 0 to (_WIDTH - 1)  do
+      Printf.printf "%s" (int_to_string(get_item board x y));
+    done;
+    Printf.printf "\n";
+  done;
+  flush stdout;;  
 
 let is_complete board = 
   let total_marbles = ref 0 in
-  for y = 0 to _HEIGHT do 
-    for x = 0 to _WIDTH  do
+  for y = 0 to (_HEIGHT - 1) do 
+    for x = 0 to (_WIDTH - 1)  do
       if ((get_item board x y) == _MARBLE_) then (
         total_marbles := (!total_marbles) + 1;
       )
     done;
   done;
-  (!total_marbles) == 1;;
+  (!total_marbles) < 4;;
 
 let apply_move board x1 y1 x2 y2 =
-  (*Printf.printf "(%d, %d) -> (%d, %d)\n" x1 y1 x2 y2;*)
+  (* Printf.printf "(%d, %d) -> (%d, %d)\n" x1 y1 x2 y2; *)
   let new_board = ref [] in
   let new_row = ref [] in
   let middle_x = ((x1 + x2) / 2) in
   let middle_y = ((y1 + y2) / 2) in
-  for y = 0 to _HEIGHT do 
-    for x = 0 to _WIDTH do
+  for y = 0 to (_HEIGHT - 1) do 
+    for x = 0 to (_WIDTH - 1) do
       if ((x == x1) && (y == y1)) then 
         new_row := _EMPTY__ :: (!new_row)
       else (
@@ -96,7 +113,7 @@ let get_moves board x y =
         moves := (x - 2, y) :: (!moves);
       )
     );
-    if ((x + 2) <= _WIDTH) then (
+    if ((x + 2) < _WIDTH) then (
       let jump_over = get_item board (x + 1) y in
       let target = get_item board (x + 2) y in
       if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
@@ -110,7 +127,7 @@ let get_moves board x y =
         moves := (x, y - 2) :: (!moves);
       )
     );
-    if ((y + 2) <= _HEIGHT) then (
+    if ((y + 2) < _HEIGHT) then (
       let jump_over = get_item board x (y + 1) in
       let target = get_item board x (y + 2) in
       if ((jump_over == _MARBLE_) && (target == _EMPTY__)) then (
@@ -120,25 +137,30 @@ let get_moves board x y =
   );
   moves;;
   
-let rec solve board =   
+let rec solve board n =
   (*let str = input_line stdin in*)
   if (is_complete board) then (
     draw_board board;
-    true;
+    raise (SolitaireException ("It worked"));
   ) else (
-    for y = 0 to _HEIGHT do 
-      for x = 0 to _WIDTH do
+    for y = 0 to (_HEIGHT - 1) do 
+      for x = 0 to (_WIDTH - 1) do
+        if (n < 3) then (
+          Printf.printf "%d - %d,%d\n" n x y;
+          flush stdout;
+        );
+
         let possible_moves = get_moves board x y in
         for i = 0 to ((List.length (!possible_moves)) - 1) do
           let (x2, y2) = List.nth (!possible_moves) i in
           let new_board = apply_move board x y x2 y2 in
-          solve (!new_board)
+          solve (!new_board) (n + 1)
         done;
       done;
     done;
     false;
   );;
-
+  
 let do_stuff =
   draw_board start_board;
   solve start_board 0;;
