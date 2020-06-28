a374a8ba7cc9951f77a2ff65cb822a3573a5ad39
James Dorrington <jdorrington@hotmail.com>
2020-06-28 12:09:05 +0100

    Tidying and moving height/width to parameters rather than globals

--- src/solitaire.ml
+++ src/solitaire.ml
@@ -4,10 +4,11 @@ let void = 0;;
 let empty = 1;;
 let marble = 2;;
 
-let _HEIGHT = 7;;
-let _WIDTH = 7;;
+(********************************************************************************)
 
-let start_board = [[void;   void;   marble; marble; marble; void;   void];
+let large_board_width = 7;;
+let large_board_height = 7;;
+let large_board = [[void;   void;   marble; marble; marble; void;   void];
                    [void;   void;   marble; marble; marble; void;   void];
                    [marble; marble; marble; marble; marble; marble; marble];
                    [marble; marble; marble; empty;  marble; marble; marble];
@@ -15,35 +16,45 @@ let start_board = [[void;   void;   marble; marble; marble; void;   void];
                    [void;   void;   marble; marble; marble; void;   void];
                    [void;   void;   marble; marble; marble; void;   void]];;
 
-(*
-let _HEIGHT = 5;;
-let _WIDTH = 5;;
+(********************************************************************************)
 
-let start_board = [[void;   marble; marble; marble; void];
-                   [marble; marble; marble; marble; marble];
-                   [marble; marble; empty;  marble; marble];
-                   [marble; marble; marble; marble; marble];
-                   [void;   marble; marble; marble; void]];;
-*)
+let medium_board_width = 5;;
+let medium_board_height = 5;;
+let medium_board = [[void;   marble; marble; marble; void];
+                    [marble; marble; marble; marble; marble];
+                    [marble; marble; empty;  marble; marble];
+                    [marble; marble; marble; marble; marble];
+                    [void;   marble; marble; marble; void]];;
+
+(********************************************************************************)
 
 let int_to_string i =
   if (i == marble) then  " O " else (
     if (i == empty) then " - " else "   ";
   );;
   
-let get_row b y = List.nth b y;;  
-let get_item b x y = List.nth (get_row b y) x;;
+(********************************************************************************)
 
-let draw_board board = 
+let get_row board y =
+  List.nth board y;;  
+  
+(********************************************************************************)
+
+let get_item board x y = 
+  List.nth (get_row board y) x;;
+
+(********************************************************************************)
+
+let draw_board board width height = 
   Printf.printf "  ";
-  for x = 0 to (_WIDTH - 1) do
+  for x = 0 to (width - 1) do
     Printf.printf " %d " x;
   done;
   Printf.printf "\n";
     
-  for y = 0 to (_HEIGHT - 1) do 
+  for y = 0 to (height - 1) do 
     Printf.printf "%d " y;
-    for x = 0 to (_WIDTH - 1)  do
+    for x = 0 to (width - 1)  do
       Printf.printf "%s" (int_to_string(get_item board x y));
     done;
     Printf.printf "\n";
@@ -51,10 +62,12 @@ let draw_board board =
   Printf.printf "\n";
   flush stdout;;  
 
-let is_complete board = 
+(********************************************************************************)
+
+let is_complete board width height = 
   let total_marbles = ref 0 in
-  for y = 0 to (_HEIGHT - 1) do 
-    for x = 0 to (_WIDTH - 1)  do
+  for y = 0 to (height - 1) do 
+    for x = 0 to (width - 1)  do
       if ((get_item board x y) == marble) then (
         total_marbles := (!total_marbles) + 1;
       )
@@ -62,14 +75,16 @@ let is_complete board =
   done;
   (!total_marbles) == 1;;
 
-let apply_move board x1 y1 x2 y2 =
+(********************************************************************************)
+
+let apply_move board width height x1 y1 x2 y2 =
   (* Printf.printf "(%d, %d) -> (%d, %d)\n" x1 y1 x2 y2; *)
   let new_board = ref [] in
   let new_row = ref [] in
   let middle_x = ((x1 + x2) / 2) in
   let middle_y = ((y1 + y2) / 2) in
-  for y = 0 to (_HEIGHT - 1) do 
-    for x = 0 to (_WIDTH - 1) do
+  for y = 0 to (height - 1) do 
+    for x = 0 to (width - 1) do
       if ((x == x1) && (y == y1)) then 
         new_row := empty :: (!new_row)
       else (
@@ -90,19 +105,9 @@ let apply_move board x1 y1 x2 y2 =
   done;
   new_board;;
   
-(* Do we still need this function? *)
-(*
-let make_move board x1 y1 x2 y2 = 
-  let move_piece = get_item board x1 y1 in
-  let target_pos = get_item board x2 y2 in
-  if (move_piece != marble) then (raise (SolitaireException ("(" ^ string_of_int(x1) ^ ", " ^ string_of_int(y1) ^ ") is not a marble" ))) else (
-    if (target_pos != empty) then (raise (SolitaireException ("(" ^ string_of_int(x2) ^ ", " ^ string_of_int(y2) ^ ") is not empty" ))) else (
-      apply_move board x1 y1 x2 y2;
-    );
-  );;
-  *)
+(********************************************************************************)
 
-let get_moves board x y =
+let get_moves board width height x y =
   let moves = ref [] in
   let move_piece = get_item board x y in
   if (move_piece == marble) then (
@@ -113,7 +118,7 @@ let get_moves board x y =
         moves := (x - 2, y) :: (!moves);
       )
     );
-    if ((x + 2) < _WIDTH) then (
+    if ((x + 2) < width) then (
       let jump_over = get_item board (x + 1) y in
       let target = get_item board (x + 2) y in
       if ((jump_over == marble) && (target == empty)) then (
@@ -127,7 +132,7 @@ let get_moves board x y =
         moves := (x, y - 2) :: (!moves);
       )
     );
-    if ((y + 2) < _HEIGHT) then (
+    if ((y + 2) < height) then (
       let jump_over = get_item board x (y + 1) in
       let target = get_item board x (y + 2) in
       if ((jump_over == marble) && (target == empty)) then (
@@ -137,25 +142,29 @@ let get_moves board x y =
   );
   moves;;
   
-let rec solve board =
+(********************************************************************************)
+
+let rec solve board width height =
   (*let str = input_line stdin in*)
-  if (is_complete board) then (
-    draw_board board;
+  if (is_complete board width height) then (
+    draw_board board width height;
     raise (SolitaireException ("It worked"));
   ) else (
-    for y = 0 to (_HEIGHT - 1) do 
-      for x = 0 to (_WIDTH - 1) do
-        let possible_moves = get_moves board x y in
+    for y = 0 to (height - 1) do 
+      for x = 0 to (width - 1) do
+        let possible_moves = get_moves board width height x y in
         for i = 0 to ((List.length (!possible_moves)) - 1) do
           let (x2, y2) = List.nth (!possible_moves) i in
-          let new_board = apply_move board x y x2 y2 in
-          solve (!new_board)
+          let new_board = apply_move board width height x y x2 y2 in
+          solve (!new_board) width height;
         done;
       done;
     done;
     false;
   );;
   
+(********************************************************************************)
+
 let do_stuff =
-  draw_board start_board;
-  solve start_board;;
+  draw_board medium_board medium_board_width medium_board_height;
+  solve medium_board medium_board_width medium_board_height;;
